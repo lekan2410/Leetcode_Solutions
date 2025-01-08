7aa32d98feeb756f70cf4395b2b83deb4d7b4627
Olalekan Ogunbiyi <107170973+lekan2410@users.noreply.github.com>
2025-01-08 23:03:34 +0000

    Update two_sum_hash.py

--- array_hash/two_sum/two_sum_hash.py
+++ array_hash/two_sum/two_sum_hash.py
@@ -2,10 +2,12 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #Dictionary
         h = {}
+
         
         for i, n in enumerate(nums):
             diff = target - n
 
+            #Checks if difference is in dictionary
             if diff in h:
                 return [h[diff], i]
             h[n] = i
