2c806c018f5885418c9db8db307992b2857481ec
Olalekan Ogunbiyi <107170973+lekan2410@users.noreply.github.com>
2025-01-09 22:27:55 +0000

    Update anagrams.py

--- array_hash/anagram/anagrams.py
+++ array_hash/anagram/anagrams.py
@@ -1,5 +1,11 @@
-
 strs = ["Jesus","eat","tea","tan","ate","nat","bat"]
 
 listdict = defaultdict(list)
 
+for str in strs:
+    
+    new_string = ''.join(sorted(str))
+    
+    listdict[new_string].append(str)
+
+print(list(listdict.values()))
