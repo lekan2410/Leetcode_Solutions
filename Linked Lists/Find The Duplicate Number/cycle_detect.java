52a4896482e584575f90979ba2463310acf5fb32
Olalekan Ogunbiyi <107170973+lekan2410@users.noreply.github.com>
2025-03-15 15:10:31 +0000

    Update cycle_detect.java

--- Linked Lists/Find The Duplicate Number/cycle_detect.java
+++ Linked Lists/Find The Duplicate Number/cycle_detect.java
@@ -1,3 +1,17 @@
 class Solution {
-   come on i n
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet();
+        ListNode root = head;
+
+        while (root != null) {
+            if (set.contains(root)) {
+                return true;
+            }
+
+            set.add(root);
+            root = root.next;
+        }
+
+        return false;
+    }
 }
